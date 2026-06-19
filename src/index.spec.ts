e90b757220debaf3660f7d7fd39110b90c9520e6
LloydLim1 <111641482+LloydLim1@users.noreply.github.com>
2026-06-19 20:52:05 +0800

    chore: initialize project scaffold

---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { SERVICE_NAME } from './index';
+
+describe('tworepo-frontend', () => {
+  it('should export SERVICE_NAME', () => {
+    expect(SERVICE_NAME).toBe('tworepo-frontend');
+  });
+});
